899d816c611e9bb6eff754b0c41355664a22daa9
saifullasyed <saifulla.syed24@sasi.ac.in>
2026-08-12 19:28:02 +0530

    Time: 0 ms (100%), Space: 11 MB (72%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string temp;
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+
+                if (temp == "..") {
+                    if (!st.empty())
+                        st.pop_back();
+                }
+                else if (!temp.empty() && temp != ".") {
+                    st.push_back(temp);
+                }
+
+                temp = "";
+            }
+            else {
+                temp += path[i];
+            }
+        }
+
+        string result = "";
+
+        for (string dir : st) {
+            result += "/" + dir;
+        }
+
+        return result.empty() ? "/" : result;
+    }
+};
