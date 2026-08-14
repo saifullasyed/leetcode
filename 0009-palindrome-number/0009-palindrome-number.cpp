a25f6cfe6484a42e330cb07580bc62a77cd48b45
saifullasyed <saifulla.syed24@sasi.ac.in>
2026-08-14 21:14:13 +0530

    Time: 5 ms (18.74%), Space: 8.6 MB (65.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev = 0;
+        int original = x;
+
+        while(x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return rev == original;
+    }
+};
