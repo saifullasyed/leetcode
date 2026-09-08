45e9db2460c7f66729742e4435c461e7f96ffa39
saifullasyed <saifulla.syed24@sasi.ac.in>
2026-09-08 13:52:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count = 0;
+        if(n < 1000) {
+            return 0;
+        }else{
+            for(int i = 1000;i <= n;i++) {
+                if(i <= n) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
