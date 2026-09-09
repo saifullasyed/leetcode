d232e59079fbaef9dd1e41d299e37302e6ca0497
saifullasyed <saifulla.syed24@sasi.ac.in>
2026-09-09 14:07:34 +0530

    Time: 0 ms (100%), Space: 23.7 MB (82.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int FirstPos = 0;
-        for(int i = 0;i < nums.size();i++) {
+        int start = 0;
+        int n = nums.size();
+        for(int i = 0;i < n;i++) {
             if(nums[i] != 0) {
-                nums[FirstPos] = nums[i];
-                FirstPos++;
+                nums[start] = nums[i];
+                start++;
             }
         }
-        while(FirstPos < nums.size()) {
-            nums[FirstPos] = 0;
-            FirstPos++;
+        while(start < n) {
+            nums[start] = 0;
+            start++;
         }
     }
 };
