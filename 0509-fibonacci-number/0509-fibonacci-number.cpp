cead77cac85d638dc9ac3f40d898b04259c27d1d
saifullasyed <saifulla.syed24@sasi.ac.in>
2026-09-15 14:38:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1) {
-            return n;
+        int a = 0;
+        int b = 1;
+        for(int i = 0;i < n;i++) {
+            int c = a+b;
+            a = b;
+            b = c;
         }
-        return fib(n-1) + fib(n-2);
+        return a;
     }
 };
